ace7aa939674c3a099a721c52d5f59b4ac86bad7
IgnacioDemarcico <115153358+IgnacioDemarcico@users.noreply.github.com>
2023-10-23 08:38:10 -0300

    aa
    falta arreglar fetch

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = ({ creacion }) => {
+  return (
+    <article className='item'>
+      <img src='/images/default_image.png' />
+    </article>
+  )
+}
+
+export default Card;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,24 @@
-export default function Projects(){
-    return(
-        <>
-        <h1>Projects</h1>
-        </>
-        );
+import { useState, useEffect } from 'react'
+import Card from "../Components/Card"
+import DB from '../services/DB'
+
+export default function Projects() {
+
+    const [creaciones, setCreaciones] = useState([])
+
+    useEffect(() => async () => setCreaciones(await new DB().getCreaciones()), [])
+
+    useEffect(() => console.log('content', creaciones), [creaciones])
+
+    return (
+        <div>
+            <h2 className='text'>Página de creaciones</h2>
+            <p className='text'>Aquí puedes mostrar el contenido específico de tu catálogo de productos.</p>
+            <section className='lista-creaciones'>
+                {
+                    creaciones?.map((creacion, index) => <Card key={index} creacion={creacion} /> )
+                }
+            </section>
+        </div>
+    );
 }
